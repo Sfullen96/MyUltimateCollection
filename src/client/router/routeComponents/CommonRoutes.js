b2555e6f6221a6cde5f7af1c1652ec01e0a66ad5
Sam Fullen <sam.fullen@intouchnetworks.com>
2018-02-15 17:29:23 +0000

    Homepage route

--- src/client/router/routeComponents/CommonRoutes.js
+++ src/client/router/routeComponents/CommonRoutes.js
@@ -6,6 +6,7 @@ import { Pages } from "../";
 export const CommonRoutes = ( props ) =>
     <Switch>
         <AuthenticatedRoute exact path="/" component={ Pages.Music } />
+        <AuthenticatedRoute exact path="/home" component={ Pages.Music } />
         <Route exact path="/login" component={ Pages.Login } />
         <Route component={ Pages.NotFound } />
     </Switch>;
